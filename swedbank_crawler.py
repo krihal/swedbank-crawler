8b6d19d771f0425bdba7488e71d220173a713c9f
Kristofer Hallin <kristofer.hallin@gmail.com>
2013-03-30 16:33:35 +0100

    Use SQLite instead of CSV files

--- swedbank_crawler.py
+++ swedbank_crawler.py
@@ -10,18 +10,20 @@ import getopt
 import datetime
 import mechanize
 import cookielib
+
 from pyquery import PyQuery
+from sqlite3 import dbapi2 as sqlite3
 
 class SwedbankCrawler(object):
     def __init__(self, username, password):
         self.username = username
         self.password = password
-        self.url = "https://internetbank.swedbank.se/bviPrivat/privat?ns=1"
+        self.url = 'https://internetbank.swedbank.se/bviPrivat/privat?ns=1'
         self.filename = ""
         self.response = ""
         self.cj = cookielib.LWPCookieJar()
         self.br = mechanize.Browser()
-        self.date = datetime.datetime.now().strftime("%m-%d")
+        self.date = datetime.datetime.now().strftime('%m-%d')
 
     def crawl(self):
         try:
@@ -32,15 +34,15 @@ class SwedbankCrawler(object):
             br.select_form(nr=0)
             br.submit()
             br.select_form(nr=1)
-            br["auth:kundnummer"] = self.username
-            br["auth:metod_2"] = ["PIN6"]
+            br['auth:kundnummer'] = self.username
+            br['auth:metod_2'] = ['PIN6']
             br.submit()
             br.select_form(nr=1)
-            br["form:pinkod"] = self.password
+            br['form:pinkod'] = self.password
             br.submit()
             br.select_form(nr=0)
             br.submit()
-            for caption in ["Inneh", "versikt", "apitalspar"]:
+            for caption in ['Inneh', 'versikt', 'apitalspar']:
                 req = br.click_link(text_regex = re.compile(caption))
                 br.open(req)
         except Exception, e:
@@ -48,47 +50,62 @@ class SwedbankCrawler(object):
 
         self.response = br.response().read()
 
-    def __fund_name__(self, str):
-        if re.search(".+>Andelar</span>.+", str):
+    def init_db(self):
+        db = self.__get_db()
+        with open('schema.sql') as fd:
+            db.cursor().executescript(fd.read())
+            db.commit()
+
+    def show_entries(self):
+        db = self.__get_db()
+        cur = db.execute('select * from swedbank')
+        entries = cur.fetchall()
+        return entries
+
+    def __get_db(self):
+        sqlite_db = sqlite3.connect('swedbank.db')
+        sqlite_db.row_factory = sqlite3.Row
+        return sqlite_db
+
+    def __fund_name(self, str):
+        if re.search('.+>Andelar</span>.+', str):
             return
         self.lastfund = str.strip()
 
-    def __print_fund_value__(self, str):
-        fund = re.findall("\"[\w_]+\"", str)
-        value = re.findall(">[\d\s\-]+,\d+<", str)
+    def __print_fund_value(self, str):
+        fund = re.findall('\"[\w_]+\"', str)
+        value = re.findall('>[\d\s\-]+,\d+<', str)
 
         if not fund or not value:
             return
         
-        fund = fund[0].translate(None, "\"")
-        value = value[0].translate(None, "<> ")
+        fund = fund[0].translate(None, '\"')
+        value = value[0].translate(None, '<> ')
 
         if re.match('\w+_verde$', fund):
-            self.__write_file__(self.lastfund, ("%s;%s;%s" % (self.date, self.lastfund, value.replace(",", "."))))
-
-    def __write_file__(self, filename, str):
-        with open("data/" + filename.replace(" ", "_"), "a") as fd:
-            fd.write(str + "\n")
-            print "Writing file: %s" % filename
+            db = self.__get_db()
+            cur = db.execute('insert into swedbank (value, name) values(?, ?)', 
+                             (value.replace(',', '.'), self.lastfund))
+            db.commit()
 
     def parse(self):
         linecnt = 0
-        lines = self.response.split("\n")
+        lines = self.response.split('\n')
         for line in lines:
             linecnt += 1
 
-            match = re.search("td headers.+>.+[\w\d]+<", line)
+            match = re.search('td headers.+>.+[\w\d]+<', line)
             if match:
-                self.__print_fund_value__(line)
+                self.__print_fund_value(line)
                 continue
 
-            match = re.search("headers=\"rentefond_fond|aktiefond_fond|altplac_fond\">(\w+|\s+|\w)+", line)
+            match = re.search('headers=\"rentefond_fond|aktiefond_fond|altplac_fond\">(\w+|\s+|\w)+', line)
             if match:            
-                self.__fund_name__(lines[linecnt])
+                self.__fund_name(lines[linecnt])
                 continue
             
 def usage():
-    print "swedbank_crawler.py -u <username> -p <password>"
+    print 'swedbank_crawler.py -u <username> -p <password>'
     sys.exit(2)
 
 def main(argv):
@@ -97,19 +114,22 @@ def main(argv):
 
     username = None
     password = None
+    init_db = 0
 
     try:
-        opts, args = getopt.getopt(argv, "hu:p:", ["username=", "password=", "help"])
+        opts, args = getopt.getopt(argv, 'ihu:p:', ['init=', 'username=', 'password=', 'help'])
     except getopt.GetoptError:
         usage()        
 
     for opt, arg in opts:
-        if opt == "-h":
+        if opt == '-h':
             usage()
-        elif opt in ("-u", "--user"):
+        elif opt in ('-u', '--user'):
             username = arg
-        elif opt in ("-p", "--password"):
+        elif opt in ('-p', '--password'):
             password = arg
+        elif opt in ('-i', '--init'):
+            init_db = 1
         else:
             usage()
 
@@ -118,10 +138,15 @@ def main(argv):
 
     try:
         swedbank = SwedbankCrawler(username, password)
+
+        if init_db:
+            swedbank.init_db()
+
         swedbank.crawl()
         swedbank.parse()
+        print swedbank.show_entries()
     except Exception, e:
-        print "Failed with error: %s" % e
+        print 'Failed with error: %s' % e
 
 if __name__ == '__main__':
     main(sys.argv[1:])
